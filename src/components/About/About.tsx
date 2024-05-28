00578d1f06ed24c27b95f759babf0463eb16b93a
bekhzod-olimov <bekhzod.olimov@gmail.com>
2024-05-28 09:44:16 +0900

    about updates

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -26,14 +26,6 @@ export function About() {
             Hi! I am Bekhzod, an experienced AI/ML engineer with a PhD degree in Computer Science & Engineering. My passion is to apply AI techniques in various applications to make people's life easier and more comfortable.
           </p>
         </ScrollAnimation>
-        {/* <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
-          <p>
-            But my services go beyond just custom development - I'm also proficient in using CMS systems like WordPress and Shopify, making it easy for my clients to take control of their own websites and keep them up to date.</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
-          <p>
-            So if you're in need of a new website or just looking to revamp your current online presence, I'd love to chat and see how I can help. Let's bring your website dreams to reality together!</p>
-        </ScrollAnimation> */}
         <ScrollAnimation animateIn="fadeInLeft" delay={0.11 * 1000}>
           <h3>Here are my main skills:</h3>
         </ScrollAnimation>
@@ -54,12 +46,13 @@ export function About() {
               <img src={tensorflow} alt="TensorFlow" />
             </ScrollAnimation>
           </div>
+          </p>
+          <p>
           <div className="hability">
             <ScrollAnimation animateIn="fadeInUp" delay={0.15 * 1000}>
               <img src={pl} alt="PyTorch Lightning" />
             </ScrollAnimation>
           </div></p>
-          <p>
           <div className="hability">
             <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
               <img src={opencv} alt="OpenCV" />
@@ -70,6 +63,8 @@ export function About() {
               <img src={sklearn} alt="Scikit-learn" />
             </ScrollAnimation>
           </div>
+          </p>
+          <p>
           <div className="hability">
             <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
               <img src={numpy} alt="NumPy" />
@@ -79,13 +74,14 @@ export function About() {
             <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
               <img src={django} alt="Django" />
             </ScrollAnimation>
-          </div></p>
-          <p>
+          </div>
           <div className="hability">
             <ScrollAnimation animateIn="fadeInUp" delay={0.20 * 1000}>
               <img src={docker} alt="Docker" />
             </ScrollAnimation>
           </div>
+          </p>
+          <p>
           <div className="hability">
             <ScrollAnimation animateIn="fadeInUp" delay={0.21 * 1000}>
               <img src={git} alt="Git" />
